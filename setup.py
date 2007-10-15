a4422161f3468b02bfcd1019b14622afec4bc95e
ianb <ianb@localhost>
2007-10-14 22:13:51 -0400

    [svn r3065] initial layout, packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.8'
+
+doc_dir = os.path.join(os.path.dirname(__file__), 'docs')
+index_file = os.path.join(doc_dir, 'index.txt')
+
+setup(name='MiniMock',
+      version=version,
+      description="The simplest possible mock library",
+      long_description=open(index_file).read().strip(),
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Testing",
+      ],
+      keywords='mock testing unittest',
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='http://pypi.python.org/pypi/MiniMock',
+      license='MIT',
+      #packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      py_modules=['minimock'],
+      zip_safe=True,
+      )
