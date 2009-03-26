60876ceff82d37b0ef0727dab6938d1ca74480f0
jab <jab@localhost>
2009-03-26 13:30:10 -0400

    [svn r3834] * it looks like PyPI doesn't support rst include directives (filed https://sourceforge.net/tracker/?func=detail&aid=2714958&group_id=66150&atid=513504). change documentation structure and include changelog programmatically in setup.py. * add mailing list as maintainer_email.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.2.2'
+version = '1.2.3'
 
 doc_dir = os.path.join(os.path.dirname(__file__), 'docs')
-index_file = os.path.join(doc_dir, 'index.txt')
+index = open(os.path.join(doc_dir, 'index.txt')).read().strip()
+changelog = open(os.path.join(doc_dir, 'changelog.txt')).read().strip()
+long_description = '\n'.join((index, changelog))
 
 setup(name='MiniMock',
       version=version,
       description="The simplest possible mock library",
-      long_description=open(index_file).read().strip(),
+      long_description=long_description,
       classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
@@ -18,6 +20,8 @@ setup(name='MiniMock',
       keywords='mock testing unittest',
       author='Ian Bicking',
       author_email='ianb@colorstudy.com',
+      maintainer='Josh Bronson',
+      maintainer_email='minimock-dev@googlegroups.com',
       url='http://pypi.python.org/pypi/MiniMock',
       license='MIT',
       #packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
